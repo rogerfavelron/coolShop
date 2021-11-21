5f3ffdf5cb1ecd674fc081155fd5d40412ef34b8
roger <rogerfavelron@gmail.com>
2021-11-21 17:52:10 +0300

    Updates

---
+++ precache-manifest.f240da45987f2abd72f5119a5ab8c6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1daa39b2fc1666e216c906b07c25d2ce",
+    "url": "/coolShop/index.html"
+  },
+  {
+    "revision": "b724e8573f962a7d408b",
+    "url": "/coolShop/static/css/main.8e34dd01.chunk.css"
+  },
+  {
+    "revision": "f149f837079d538abc0e",
+    "url": "/coolShop/static/js/2.353ada61.chunk.js"
+  },
+  {
+    "revision": "e364af75fff56a5298ee87761858e88e",
+    "url": "/coolShop/static/js/2.353ada61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b724e8573f962a7d408b",
+    "url": "/coolShop/static/js/main.36e0e565.chunk.js"
+  },
+  {
+    "revision": "696e413eddffbb089f56",
+    "url": "/coolShop/static/js/runtime-main.338e6688.js"
+  }
+]);
